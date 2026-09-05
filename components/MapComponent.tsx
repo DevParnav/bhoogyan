5b32584fb83ebd686857fdbb758345b3b2410ba1
Parnav00 <parnavyadav002@gmail.com>
2026-09-06 05:28:37 +0530

    fix: resolve MapComponent syntax error breaking Vercel build

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -91,7 +91,7 @@ export default function MapComponent({ onAoiCreated, onAoiCleared }: MapComponen
     <MapContainer 
       center={[18.5204, 73.8567]} // Pune coordinates
       zoom={11} 
-      style={{ position: \'absolute\', top: 0, bottom: 0, left: 0, right: 0, zIndex: 0 }}
+      style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, zIndex: 0 }}
       zoomControl={true}
     >
       <TileLayer
